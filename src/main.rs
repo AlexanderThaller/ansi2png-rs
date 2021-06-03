a568f0df1ec72b8773755bfa1a0168977881e1c4
Alexander Thaller <alexander.thaller@trivago.com>
2021-06-03 13:52:10 +0200

    Add colors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ansi_parser::{
     AnsiParser,
+    AnsiSequence,
     Output,
 };
 use image::{
@@ -13,15 +14,24 @@ use rusttype::{
 };
 use std::path::Path;
 
+mod pallete;
+
 fn main() {
     let parsed: Vec<Output> = include_str!("../resources/out.ansi").ansi_parse().collect();
 
     let path = Path::new("output.png");
 
+    let pallete: pallete::Palette = pallete::Custom {}.into();
+
     let font = Vec::from(
         include_bytes!("../resources/dejavu-fonts-ttf-2.37/ttf/DejaVuSansMono.ttf") as &[u8],
     );
+    let font_bold = Vec::from(include_bytes!(
+        "../resources/dejavu-fonts-ttf-2.37/ttf/DejaVuSansMono-Bold.ttf"
+    ) as &[u8]);
+
     let font = Font::try_from_vec(font).unwrap();
+    let font_bold = Font::try_from_vec(font_bold).unwrap();
 
     let height = 50.0;
     let scale = Scale {
@@ -64,33 +74,58 @@ fn main() {
 
     let mut image = RgbImage::new(width, height);
 
-    let draw_x = 0;
+    // Set background
+    for (_x, _y, pixel) in image.enumerate_pixels_mut() {
+        *pixel = image::Rgb(pallete.primary_background);
+    }
+
+    let mut draw_x = 0;
     let mut draw_y = 0;
+    let mut foreground_color = Rgb(pallete.primary_foreground);
+    let mut text_bold = false;
 
-    parsed
-        .iter()
-        .filter_map(|block| {
-            if let Output::TextBlock(text) = block {
-                Some(text.to_string())
+    parsed.iter().for_each(|block| match block {
+        Output::TextBlock(text) => text.chars().for_each(|c| {
+            let draw_font = if text_bold { &font_bold } else { &font };
+
+            if c == '\n' {
+                draw_x = 0;
+                draw_y += new_line_distance
             } else {
-                None
-            }
-        })
-        .collect::<String>()
-        .lines()
-        .for_each(|line| {
-            draw_text_mut(
-                &mut image,
-                Rgb([255u8, 255u8, 255u8]),
-                draw_x,
-                draw_y,
-                scale,
-                &font,
-                &line,
-            );
+                draw_text_mut(
+                    &mut image,
+                    foreground_color,
+                    draw_x,
+                    draw_y,
+                    scale,
+                    draw_font,
+                    &c.to_string(),
+                );
 
-            draw_y += new_line_distance;
-        });
+                let advance_width = font.glyph(c).scaled(scale).h_metrics().advance_width;
+                draw_x += advance_width as u32;
+            }
+        }),
+
+        Output::Escape(escape) => {
+            if let AnsiSequence::SetGraphicsMode(values) = escape {
+                for value in values {
+                    match value {
+                        0 => {
+                            text_bold = false;
+                            foreground_color = Rgb(pallete.primary_foreground)
+                        }
+                        1 => text_bold = true,
+                        31 => foreground_color = Rgb(pallete.foreground_red),
+                        34 => foreground_color = Rgb(pallete.foreground_blue),
+                        _ => {
+                            dbg!("not implemented for {}", value);
+                        }
+                    }
+                }
+            }
+        }
+    });
 
     let _ = image.save(path).unwrap();
 }
---
+++ src/pallete.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+pub struct Palette {
+    pub primary_background: [u8; 3],
+    pub primary_foreground: [u8; 3],
+    pub foreground_blue: [u8; 3],
+    pub foreground_red: [u8; 3],
+}
+
+pub struct Custom {}
+
+impl From<Custom> for Palette {
+    fn from(_: Custom) -> Self {
+        Palette {
+            // primary_background: "0x161616".parse().unwrap(),
+            // primary_foreground: "0xf2f2f2".parse().unwrap(),
+            primary_foreground: [242, 242, 242],
+            primary_background: [22, 22, 22],
+            foreground_blue: [21, 101, 193],
+            foreground_red: [198, 40, 40],
+        }
+    }
+}
